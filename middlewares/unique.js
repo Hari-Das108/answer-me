6c0dbb2a45f5a6deb138dfb893460fd1518a66de
Hari-Das108 <cocsupercell393@gmail.com>
2025-11-24 12:20:15 +0530

    Removed unique.js

--- middlewares/unique.js
+++
@@ -1,6 +0,0 @@
-export const unique = async (req, res, next) => {
-  console.log("Unique middleware triggered");
-  console.log("User-Agent:", req.get("User-Agent"));
-  console.log("IP:", req.ip);
-  next();
-};
--- routes/apiKeyRoutes.js
+++ routes/apiKeyRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import apiKeyController from "../controllers/apiKeyController.js";
 import { apiKeyLimiter } from "../middlewares/rateLimiter.js";
-import { unique } from "../middlewares/unique.js";
 import cors from "cors";
 
 const router = express.Router();
@@ -10,7 +9,6 @@ router
   .route("/generate-key")
   .get(
     cors({ exposedHeaders: ["Retry-After"] }),
-    unique,
     apiKeyLimiter,
     apiKeyController.generateKey
   );
