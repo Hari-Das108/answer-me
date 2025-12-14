e7090150f8255948de87d1bc8b4e3c9a8bfe0e35
Hari-Das108 <cocsupercell393@gmail.com>
2025-12-14 19:30:13 +0530

    cors fix 7

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // 4. Use the SAME config for Preflight (OPTIONS) requests
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
