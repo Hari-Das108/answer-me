1bafe2f0792787ef79868fb7f986ee5bc0ff4d5d
Hari-Das108 <cocsupercell393@gmail.com>
2025-12-19 00:55:33 +0530

    production error fix

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -38,7 +38,7 @@ export default (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    let error = { ...err };
+    let error = err;
 
     if (error.code === 11000) {
       error = handleDuplicateErrorDB(error);
